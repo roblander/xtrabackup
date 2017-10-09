bb082dafdf5d7a5e35fb2bea8f0e8382fb4f8b36
Yura <qw4n7y@gmail.com>
2017-10-09 17:57:39 +0300

    configuring the backup frequency by env variable

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 env | sed 's/^\(.*\)$/export \1/g' > /root/envs.sh
 chmod +x /root/envs.sh
 
+# update crontab file with env parameters
+sed -i .bak -e s/__BACKUP_FREQUENCY_IN_DAYS__/$BACKUP_FREQUENCY_IN_DAYS/g /etc/cron.d/hello-cron
+
 # Run the command on container startup
 cron && tail -f /var/log/cron.log
