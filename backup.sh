728b40696e59d2326002e5cb1f7a52b8945943c2
Ivan Nesterovich <vanya.nesterovich@gmail.com>
2018-11-19 13:03:23 +0300

    Move build backup to the volume container of docker

--- backup.sh
+++ backup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+mkdir /root/backups/tmp
+mkdir /root/backups/backup
 
 xtrabackup --backup \
            --databases=$DATABASES \
            --datadir=/root/data \
            --password=$PASSWORD \
-           --target-dir=/root/backup \
+           --target-dir=/root/backups/backup \
            --host=$HOST \
            --user=$USER;
 
-BACKUP="/tmp/backup-$(date +%Y-%m-%d-%H-%M)-${DATABASES// /_}.tar.gz";
+BACKUP="/root/backups/tmp/backup-$(date +%Y-%m-%d-%H-%M)-${DATABASES// /_}.tar.gz";
 
-tar -zcvf $BACKUP /root/backup;
-rm -rf /root/backup/*;
+tar -zcvf $BACKUP /root/backups/backup;
+rm -rf /root/backups/backup/*;
 
 # using `cp` + `rm` but not `mv` bacause of
 # mv tries to preserve file's ownership which bangs with attached docker volumes for me
